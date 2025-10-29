29573f7bc4cd9e51169e6ecb284427938b7562d2
FrederickWennborg <frederick.wennborg@systementor.se>
2025-10-29 13:32:14 +0100

    Added new customer class.

---
+++ MyGitProject/Customer'.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyGitProject
+{
+    internal class Customer_
+    {
+        public int Id { get; set; }     
+        public int Name { get; set; }
+        public int City { get; set; }
+
+
+
+
+    }
+}
